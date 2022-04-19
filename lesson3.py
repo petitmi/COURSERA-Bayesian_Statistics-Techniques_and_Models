67298c5563d7b69c1a1e4fb05eef170819adda97
petitmi <43694291+petitmi@users.noreply.github.com>
2022-04-19 09:44:37 +0800

    Update lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -1,19 +1,27 @@
-import numpy as np
-from scipy.stats import beta
 import matplotlib.pyplot as plt
-#  question5
-a=5
-b=3
+from scipy import stats
+import numpy as np
+import pandas as pd
+a, b = 5, 3
+# draw dist and pdf
 fig, ax = plt.subplots(1, 1)
-# Generate samples
-r = beta.rvs(a, b, size=10000)
-r_mean=np.average(r)
-odds=r_mean/(1-r_mean)
-print('odds: %.1f' % odds)
-ax.hist(r, density=True, histtype='stepfilled',bins=100, alpha=0.6)
+x = np.linspace(stats.beta.ppf(0.01, a, b),
+                stats.beta.ppf(0.99, a, b), 100)
+ax.plot(x, stats.beta.pdf(x, a, b),
+       'r-', lw=5, alpha=0.6, label='beta pdf')
+rv = stats.beta(a, b)
+ax.plot(x, rv.pdf(x), 'k-', lw=2, label='frozen pdf')
+r = stats.beta.rvs(a, b, size=10000)
+ax.hist(r, density=True, histtype='stepfilled', alpha=0.8)
 ax.legend(loc='best', frameon=False)
-x = np.linspace(beta.ppf(0.01, a, b),
-                beta.ppf(0.99, a, b), 100)
-ax.plot(x, beta.pdf(x, a, b),
-        lw=5, alpha=0.6, label='beta pdf')
 plt.show()
+#question5
+A_q5=np.average(r/(1-r))
+print(A_q5)
+#question6
+r_pd=pd.DataFrame(r)
+A_q6=r_pd[r_pd>0.5].count()/r_pd.count()
+print(A_q6)
+#question7
+A_q7=stats.norm().ppf(0.3)
+print(A_q7)
